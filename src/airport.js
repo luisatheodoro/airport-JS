0eaf97843c8725451b4beea96d5b2fdd7d01fedb
luisatheodoro <theodoro.luisa@gmail.com>
2019-02-07 11:54:20 +0000

    refactoring instructPlaneToTakeOff method

--- src/airport.js
+++ src/airport.js
@@ -20,12 +20,29 @@ Airport.prototype = {
   },
 
   instructPlaneToTakeOff: function(plane) {
-    if (this.weather.getWeather() === 'Stormy') {
-      throw new Error(`${plane} cannot take off due to bad weather`)
-    }else if (this.hangar.indexOf(plane) === -1) {
+    this._isNotParked(plane);
+    this._throwErrorIfStormy(plane, "take off");
+    this._removePlaneFromHangar(plane);
+    return true;
+  },
+
+  _isNotParked: function(plane) {
+    if (this._findPlaneIndex(plane) === -1){
       throw new Error(`${plane} cannot take off as this plane is not in this airport`)
     }
-    this.hangar.splice(this.hangar.indexOf(plane), 1);
-    return true;
-  }
+  },
+
+  _findPlaneIndex: function(plane) {
+    return this.hangar.indexOf(plane)
+  },
+
+  _throwErrorIfStormy: function(plane, situation) {
+    if (this.weather.isStormy()) {
+      throw new Error(`${plane} cannot ${situation} due to bad weather`)
+    }
+  },
+
+  _removePlaneFromHangar: function(plane) {
+    this.hangar.splice(this._findPlaneIndex(plane), 1);
+  },
 };
